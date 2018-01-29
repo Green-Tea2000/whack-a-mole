b151b3820bff40246be49b11bf8f7fe305cc1a95
Patricia G Raftery <praftery8@gmail.com>
2018-01-29 14:32:08 -0800

    local storage for name not working

--- js/aux.js
+++ js/aux.js
@@ -1,5 +1,24 @@
 'use strict';
 
-var playerName = document.getElementById('playerNameInput');
+var playerName = document.getElementById('player-Name-Input');
+var addPlayerButton = document.getElementById('addPlayer');
+console.log(playerName);
 
-JSON.parse(localStorage.playerName);
+
+//Add Player Name and Send to LOCAL STORAGE Event
+function addAPlayerName(event) {
+  event.preventDefault();
+
+  localStorage.setItem('localStoragePlayerName', JSON.stringify(playerName));
+
+  console.log(playerName);
+  if (localStorage.localStoragePlayerName) {
+    alert('Ready to whack some moles???');
+
+  }
+}
+
+addPlayerButton.addEventListener('click', addAPlayerName);
+
+localStorage.dog = 'demi';
+localStorage.test = playerName;
