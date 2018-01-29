19992d6e1f115fb5aa7e60ede82b855d23713506
haronYunis <haronyunis@icloud.com>
2018-01-29 14:01:17 -0800

    updated scores page with a small table that ready for js data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 'use strict'
+//USING THE DOM TO ACCESS THE TABLE FROM SCORE.html
+var tableId = document.getElementById('tableId');
+
+function ScoreTableHeader() {
+  // Creating the table header which consist of GAMES, SCORES & LEVEL REACHED
+  var thEl = document.createElement('th');
+  var thEl2 = document.createElement('th');
+  // var thEl3 = document.createElement('th');
+  
+  // GIVING EACH th ELEMENT CONTENT
+  thEl.textContent = 'Games'
+  thEl2.textContent = 'Score';
+  // thEl3.textContent = 'Level Reached';
+
+  // APPENDING th ELEMENT TO THE TABLE
+  tableId.appendChild(thEl)
+  tableId.appendChild(thEl2);
+  // tableId.appendChild(thEl3);
+}
+
+
+
+
+
+
+ScoreTableHeader();
