0ec3f01a74024b2e0801583e400fa09a897cd00c
joereyes0123 <rey0123@rey0123s-MacBook-Pro.local>
2018-01-29 12:02:28 -0800

    added code to app.js outlining our timeline

--- js/app.js
+++ js/app.js
@@ -1,1 +1,49 @@
 'use strict'
+var numPoints = 0;
+var raf;
+var mouseX;
+var mouseY;
+var posNegIndicator = 1;
+var nIntervId;
+
+
+//new mole image
+var imgMoleDwg =new Image();
+imgMoleDwg.src = 'mole_drawing.png';
+
+//canvas traits
+var canvasWidth = 800;
+var canvasHeight = 400;
+
+//mole pic traits
+var molePicWidth = 100;
+var molePicHeight = 150;
+var molePicOffset = 25;
+
+//     beginning of draw function
+
+
+
+//clear canvas and draw background
+
+
+//draw point counter
+
+
+//draw pic of mole on canvas
+
+//redraw frame
+
+//       end of draw function
+
+//a timed interval function that changes from Pos to Neg(used in screen indicator)
+
+//Display mole on screen if POS Neg indicator is POS
+
+//get cursor Postion
+
+//check whether click was inside mole area
+
+//Eventlistener for clicks to run corresponding functions
+
+
