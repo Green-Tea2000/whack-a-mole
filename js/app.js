da669a4d3af523c7c5ac88769bd0f69460ea0815
joereyes0123 <rey0123@rey0123s-MacBook-Pro.local>
2018-01-29 12:45:09 -0800

    added the function of individual moles; workign on this branch with Tyler from start of Day1 Monday

--- js/app.js
+++ js/app.js
@@ -20,30 +20,77 @@ var molePicWidth = 100;
 var molePicHeight = 150;
 var molePicOffset = 25;
 
-//     beginning of draw function
+//     beginning of draw 
+var canvas = document.getElementById('tutorial');
+var ctx = canvas.getContext('2d');
+function draw() {
 
 
+  //clear canvas and draw background
+  ctx.clearRect(0,0, canvasWidth, canvasHeight);
+  ctx.fillStyle = 'green';
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-//clear canvas and draw background
+  //draw point counter
+  ctx.font = '48px sans-serif';
+  ctx.fillStyle = 'white';
+  ctx.fillText('Point: ' + numPoints, 25, 375);
 
 
-//draw point counter
+  //draw pic of mole on canvas
+  drawMole();
 
-
-//draw pic of mole on canvas
-
-//redraw frame
-
+  //redraw frame
+  raf = window.requestAnimationFrame(draw);
+}
 //       end of draw function
 
+
+
 //a timed interval function that changes from Pos to Neg(used in screen indicator)
+function intervalFunc(){
+  nIntervId = setInterval(posNegAlternate, 1000);
+}
+
+
+function posNegAlternate(){
+  posNegIndicator *= -1;
+}
 
 //Display mole on screen if POS Neg indicator is POS
+function drawMole(){
+  if(posNegIndicator >= 0){
+    ctx.drawImage(imgMoleDwg, molePicOffset, molePicOffset, molePicWidth, molePicHeight);
+  }
+}
 
 //get cursor Postion
+function getCursorPosition(event){
+  var rect = canvas.getBoundingClientRect();
+  mouseX = event.clientX - rect.left;
+  mouseY = event.clientY - rect.top;
+  console.log('x: ' + mouseX + ' y: ' + mouseY);
+}
+
 
 //check whether click was inside mole area
+function hitOrMiss(){
+  if((mouseX >= molePicOffset && mouseX <= (molePicOffset + molePicWidth))
+  && (mouseY >= molePicOffset && mouseY <= (molePicOffset + molePicHeight))
+  && (posNegIndicator >= 0)){
+    console.log('hit');
+    numPoints++;
+  }else {
+    console.log('miss');
+  }
+}
 
 //Eventlistener for clicks to run corresponding functions
+canvas.addEventListener('click', function(e){
+  getCursorPosition(e);
+  hitOrMiss();
+});
+intervalFunc();
+draw();
 
 
